c161304f78e6f5507f741b04bab768303e361efa
Samimi <smokhadar@gmail.com>
2023-05-20 11:27:25 -0700

    added svg practice

---
+++ tests/cli.test.js
@@ -0,0 +1,7 @@
+const CLI = require('../lib/cli.js')
+
+describe('CLI', () => {
+    it('should be defined', () => {
+        expect(CLI).toBeDefined();
+    })
+})
---
+++ tests/svg.test.js
@@ -0,0 +1,8 @@
+describe('svg', () => {
+    it('add color', () => {
+        const shape = new Triangle();
+        shape.setColor("blue");
+        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+    }
+    )
+})
