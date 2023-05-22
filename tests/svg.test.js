abccf4ffcddf18e612a38e1ebc297f6e5d2b3389
Samimi <smokhadar@gmail.com>
2023-05-21 23:16:57 -0700

    class, generate and write funct

--- tests/svg.test.js
+++ tests/svg.test.js
@@ -1,4 +1,11 @@
 describe('svg', () => {
+    it('writeFile creates svg', () => {
+        const svg = file.svg;
+        expect(writeFile).to
+    })
+})
+
+describe('svg', () => {
     it('add color', () => {
         const shape = new Triangle();
         shape.setColor("blue");
